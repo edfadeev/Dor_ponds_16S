497486522e0be104cf2529f89348c5f876dd1e4f
edfadeev <dr.eduard.fadeev@gmail.com>
2020-09-30 15:43:21 +0200

    colours added to Alpha div

--- Dor_alpha_div.R
+++ Dor_alpha_div.R
@@ -8,7 +8,27 @@ library("reshape2"); packageVersion("reshape2")
 library("rstatix"); packageVersion("rstatix")
 
 #load colour palettes
-source('./Scripts/color_palettes.R')
+tol21rainbow<- c("#771155", 
+                 "#AA4488", 
+                 "#CC99BB", 
+                 "#114477", 
+                 "#4477AA", 
+                 "#77AADD", 
+                 "#117777", 
+                 "#44AAAA", 
+                 "#77CCCC", 
+                 "#117744", 
+                 "#44AA77", 
+                 "#88CCAA", 
+                 "#777711", 
+                 "#AAAA44", 
+                 "#DDDD77", 
+                 "#774411", 
+                 "#AA7744", 
+                 "#DDAA77", 
+                 "#771122", 
+                 "#AA4455", 
+                 "#DD7788")
 
 se <- function(x, na.rm=FALSE) {
   if (na.rm) x <- na.omit(x)
