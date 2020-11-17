460ccac3740f7404e7071a3bdf416ec06ac35c27
edfadeev <dr.eduard.fadeev@gmail.com>
2020-11-17 10:18:03 +0100

    adjusted to the manuscript outline

--- Dor_16S_dada2_preprocess.R
+++ Dor_16S_dada2_preprocess.R
@@ -58,40 +58,37 @@ names(dna) <- taxa_names(Dor_ps0)
 Dor_ps0 <- merge_phyloseq(Dor_ps0, dna)
 taxa_names(Dor_ps0) <- paste0("ASV", seq(ntaxa(Dor_ps0)))
 
+#Remove controls and etc
+Dor_ps0<- subset_samples(Dor_ps0, location %in% c("Res.","V2.","D1.") & 
+                          !comment %in% c("control.SP01","control.SP02","control.SP03", "duplicate batch 2","Ashraf did the PCR")&
+                          !Sample_number_dada2 %in% c("95"))
+
 #remove chloroplast and mitochondria reads
 Dor_ps0.chl<- subset_taxa(Dor_ps0, Order == "Chloroplast")
 Dor_ps0.mit<- subset_taxa(Dor_ps0, Family == "Mitochondria")
 
 Dor_ps0<- subset_taxa(Dor_ps0, !Order == "Chloroplast" & !Family =="Mitochondria")
 
-
-
-#Remove controls and etc
-Dor_ps<- subset_samples(Dor_ps0, location %in% c("Res.","V2.","D1.") & 
-                          !comment %in% c("control.SP01","control.SP02","control.SP03", "duplicate batch 2","Ashraf did the PCR")&
-                          !Sample_number_dada2 %in% c("95"))
-
-Dor_ps<- prune_taxa(taxa_sums(Dor_ps)>0,Dor_ps)
-
-
 #remove samples with less than 2000 sequences
-Dor_ps.prev<-  prune_samples(sample_sums(Dor_ps)>2000, Dor_ps)
+Dor_ps0<-  prune_samples(sample_sums(Dor_ps0)>2000, Dor_ps0)
+
+Dor_ps.prev<- prune_taxa(taxa_sums(Dor_ps0)>0,Dor_ps0)
 
 saveRDS(Dor_ps.prev, "./data/Dor_ps_prev.rds")
 
 #####################################
 #Plot total number of reads and OTUs per sample
 #####################################
-readsumsdf <- data.frame(nreads = sort(taxa_sums(Dor_ps), TRUE), sorted = 1:ntaxa(Dor_ps), 
+readsumsdf <- data.frame(nreads = sort(taxa_sums(Dor_ps.prev), TRUE), sorted = 1:ntaxa(Dor_ps.prev), 
                          type = "OTUs")
-readsumsdf <- rbind(readsumsdf, data.frame(nreads = sort(sample_sums(Dor_ps), 
-                                                         TRUE), sorted = 1:nsamples(Dor_ps), type = "Samples"))
+readsumsdf <- rbind(readsumsdf, data.frame(nreads = sort(sample_sums(Dor_ps.prev), 
+                                                         TRUE), sorted = 1:nsamples(Dor_ps.prev), type = "Samples"))
 title = "Total number of reads"
 p <- ggplot(readsumsdf, aes(x = sorted, y = nreads)) + geom_bar(stat = "identity")
 OTU_libs_overview <-  p + ggtitle(title) + scale_y_log10() + facet_wrap(~type, 1, scales = "free")+ 
   xlab("Samples")+ ylab("Number of reads")+ theme_bw()
 
-ggsave("./figures/libs_overview_run1.pdf", 
+ggsave("./figures/libs_overview.png", 
        plot = OTU_libs_overview,
        units = "cm",
        width = 30, height = 30, 
@@ -101,11 +98,12 @@ ggsave("./figures/libs_overview_run1.pdf",
 #####################################
 #Plot rarefaction
 ####################################
-iNEXT.out <- iNEXT(as.data.frame(otu_table(Dor_ps)), q=0, datatype="abundance")
+iNEXT.out <- iNEXT(as.data.frame(otu_table(Dor_ps.prev)), q=0, datatype="abundance")
 rare <-fortify(iNEXT.out, type=1)
 
-meta <- as(sample_data(Dor_ps), "data.frame")
+meta <- as(sample_data(Dor_ps.prev), "data.frame")
 meta$site <- rownames(meta)
+rare$Run <- meta$Run[match(rare$site, meta$site)]
 rare$Loc <- meta$location[match(rare$site, meta$site)]
 rare$Year <- meta$Year[match(rare$site, meta$site)] 
 rare$Month <- meta$Month[match(rare$site, meta$site)] 
@@ -129,7 +127,7 @@ rare.p <- ggplot(rare, aes(x=x, y=y, colour = site))+
   #ylim(0,5000)+
   theme_classic(base_size = 12)+theme(legend.position="bottom")
 
-ggsave("./figures/Run1_rarefactions.pdf", 
+ggsave("./figures/rarefactions.png", 
        plot = rare.p,
        units = "cm",
        width = 30, height = 30, 
